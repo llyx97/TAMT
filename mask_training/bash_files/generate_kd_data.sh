23ab4747f5c7df3b489a89f903a870ebffc798a4
llyx97 <57584631+llyx97@users.noreply.github.com>
2022-04-20 15:42:17 +0800

    Update generate_kd_data.sh

--- mask_training/bash_files/generate_kd_data.sh
+++ mask_training/bash_files/generate_kd_data.sh
@@ -1,11 +1,16 @@
 export root_dir=${root_directory}/mask_training
+export length=512
 
-for length in 512
-do
-	python $root_dir/pregenerate_training_data.py \
-		--train_corpus $root_dir/data/wikitext-103/wiki.train.raw \
-		--bert_model $root_dir/models/bert_pt \
-		--output_dir $root_dir/data/wikitext-103-kd/length$length \
-		--max_seq_len $length \
-		--epochs_to_generate 1 
-done
+python $root_dir/pregenerate_training_data.py \
+	--train_corpus $root_dir/data/wikitext-103/wiki.train.raw \
+	--bert_model $root_dir/models/bert_pt \
+	--output_dir $root_dir/data/wikitext-103-kd/length$length \
+	--max_seq_len $length \
+	--epochs_to_generate 1 \
+	
+python $root_dir/pregenerate_training_data.py \
+	--train_corpus $root_dir/data/wikitext-103/wiki.valid.raw \
+	--bert_model $root_dir/models/bert_pt \
+	--output_dir $root_dir/data/wikitext-103-kd/length$length/eval_data \
+	--max_seq_len $length \
+	--epochs_to_generate 1
