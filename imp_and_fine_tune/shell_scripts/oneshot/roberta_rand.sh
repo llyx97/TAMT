d4351f48cf3d5b3b2edc68157d76af0d227bdfef
llyx97 <57584631+llyx97@users.noreply.github.com>
2022-04-21 10:51:40 +0800

    Update roberta_rand.sh

--- imp_and_fine_tune/shell_scripts/oneshot/roberta_rand.sh
+++ imp_and_fine_tune/shell_scripts/oneshot/roberta_rand.sh
@@ -2,7 +2,7 @@ export ROOT_DIR=${root_directory}
 export prun_type=random
 export model_type=roberta
 
-for zero in 0.1 0.2 0.3 0.4
+for zero in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
 do
 	for seed in 1 2 3
 	do
