57d3dd34d3b8550aac1edf2fd573d38bea38ac96
llyx97 <57584631+llyx97@users.noreply.github.com>
2022-04-21 10:51:05 +0800

    Update bert_mag.sh

--- imp_and_fine_tune/shell_scripts/oneshot/bert_mag.sh
+++ imp_and_fine_tune/shell_scripts/oneshot/bert_mag.sh
@@ -1,7 +1,7 @@
 export ROOT_DIR=${root_directory}
 export model_type=bert
 
-for zero in 0.5 0.6 0.7 0.8 0.9
+for zero in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
 do
 	python $ROOT_DIR/imp_and_fine_tune/oneshot.py \
 		--model_type $model_type \
