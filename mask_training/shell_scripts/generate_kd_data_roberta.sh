a231ef32fb7ee17165ab19041b0f8e76bc8837f2
llyx97 <57584631+llyx97@users.noreply.github.com>
2022-04-21 22:54:43 +0800

    Update generate_kd_data_roberta.sh

--- mask_training/shell_scripts/generate_kd_data_roberta.sh
+++ mask_training/shell_scripts/generate_kd_data_roberta.sh
@@ -3,7 +3,7 @@ export length=512
 
 python $root_dir/pregenerate_training_data.py \
 	--train_corpus $root_dir/data/wikitext-103/wiki.train.raw \
-	--bert_model roberta-base \
+	--bert_model ${RoBERTa_BASE_DIR}$ \
 	--output_dir $root_dir/data/wikitext-103-kd/roberta \
 	--max_seq_len $length \
 	--epochs_to_generate 1 \
@@ -11,7 +11,7 @@ python $root_dir/pregenerate_training_data.py \
 	
 python $root_dir/pregenerate_training_data.py \
 	--train_corpus $root_dir/data/wikitext-103/wiki.valid.raw \
-	--bert_model roberta-base \
+	--bert_model ${RoBERTa_BASE_DIR}$ \
 	--output_dir $root_dir/data/wikitext-103-kd/roberta/eval_data \
 	--max_seq_len $length \
 	--epochs_to_generate 1 \
